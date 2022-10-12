65b40b13418d77249d9dc4cce149a7a77b6d78a8
Nestor45 <nestoralvarez97@gmail.com>
2022-10-12 11:35:17 -0500

    new fun doubleDouble

--- math.c
+++ math.c
@@ -6,5 +6,14 @@ int doubleInt(int a) {
     if (a > MAX_INT) {
         return -1;
     }
+
     return a*2;
+}
+
+double doubleDouble(double a) {
+    if (a > MAX_DOUBLE) {
+        return -1;
+    }
+    
+    return a*2:
 }
